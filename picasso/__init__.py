d29afc90bb87b9556d53e0f31abdf52a13311537
devinci-it <vince.dev@icloud.com>
2024-08-11 10:53:27 -0700

    Added the progress loader feature

--- picasso/__init__.py
+++ picasso/__init__.py
@@ -2,11 +2,11 @@ from .style_composer import StyleComposer
 from .style_formatter import StyleFormatter
 from .style_decorator import StyleDecorator
 from .utility import ColorConverter
-
-
+from .loader import ProgressLoader
 __all__ = [ 
     'StyleComposer',
     'StyleFormatter',
     'StyleDecorator',
-    'ColorConverter'
+    'ColorConverter',
+    'ProgressLoader'
 ]
